821b6f0bc44356a6a00e5773543960066673e072
David Aguirre Alonso <davidaguirrealonso@gmail.com>
2021-03-15 00:08:42 +0100

    Observables and Promises

--- src/app/pages/pages.routing.module.ts
+++ src/app/pages/pages.routing.module.ts
@@ -6,16 +6,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { GraficalComponent } from './grafical/grafical.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 const routes: Routes = [
   { 
       path: 'dashboard', 
       component: PagesComponent,
       children: [
-          { path: '', component: DashboardComponent },
-          { path: 'progress', component: ProgressComponent },
-          { path: 'grafical', component: GraficalComponent },
-          { path: 'account-settings', component: AccountSettingsComponent },
+          { path: '', component: DashboardComponent, data: { titulo: 'Dashboard'} },
+          { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress'}  },
+          { path: 'grafical', component: GraficalComponent, data: { titulo: 'Grafocal'}  },
+          { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Account Settings'}  },
+          { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas'}  },
+          { path: 'rxjs', component: RxjsComponent, data: { titulo: 'Rxjs'}  },
       ]
   },
 ];
--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -13,6 +13,8 @@ export class SidebarService {
         { titulo: 'Main', url: '/' },
         { titulo: 'ProgressBar', url: 'progress' },
         { titulo: 'Gráficas', url: 'grafical' },
+        { titulo: 'Promesas', url: 'promesas' },
+        { titulo: 'Rxjs', url: 'rxjs' },
       ]
     },
   ];
